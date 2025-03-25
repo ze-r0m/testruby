e81b123f296e863c2bc0a378535cd723ff39253d
ze-r0m <rzv2003@gmail.com>
2025-03-25 16:22:43 +0300

    style with bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  resources :users
   resources :orders do
     resources :order_items
   end
   resources :products
   resources :categories
-  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
