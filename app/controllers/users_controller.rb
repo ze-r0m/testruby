0559f01b7d7064fad56bfedb35544d5a0e21b722
ze-r0m <rzv2003@gmail.com>
2025-03-26 14:34:29 +0300

    rails ujs, fix link_to(delete)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,7 +49,7 @@ class UsersController < ApplicationController
 
   # DELETE /users/1 or /users/1.json
   def destroy
-    @user.destroy!
+    @user.destroy
 
     respond_to do |format|
       format.html { redirect_to users_path, status: :see_other, notice: "User was successfully destroyed." }
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,9 @@
 # Pin npm packages by running ./bin/importmap
+# Pin npm packages by running ./bin/importmap
 
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin "@rails/ujs", to: "rails-ujs.js" # <-- Важно!
 pin_all_from "app/javascript/controllers", under: "controllers"
